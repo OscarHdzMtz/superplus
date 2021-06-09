74b6dac32bf00804657d6a358a741572981804fa
Oscar <oskr9922@gmail.com>
2021-06-09 11:27:07 -0500

    modulo registro personalizado y funcioando al 100 para empezar con dashboard

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,11 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    /* 'failed' => 'These credentials do not match our records.', */
+    'failed' => 'Credencial incorrecto',
+    /* 'password' => 'The provided password is incorrect.', */
+    'password' => 'Contraseña Incorrecta',
+    /* 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.', */
+    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en: segundos segundos.',
 
 ];
